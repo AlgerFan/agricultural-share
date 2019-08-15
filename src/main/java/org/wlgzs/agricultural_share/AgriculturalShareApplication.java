0f579f7aab6dbe8795381e8ed2d7cbef1992f51c
AlgerFan <1052391560@qq.com>
2019-08-15 14:00:16 +0800

    最终版-增加打war包方式，易于部署

--- src/main/java/org/wlgzs/agricultural_share/AgriculturalShareApplication.java
+++ src/main/java/org/wlgzs/agricultural_share/AgriculturalShareApplication.java
@@ -2,9 +2,20 @@ package org.wlgzs.agricultural_share;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+//注释部分打war包时需要
+//@ServletComponentScan
+//public class AgriculturalShareApplication extends SpringBootServletInitializer {
 public class AgriculturalShareApplication {
+
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AgriculturalShareApplication.class);
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(AgriculturalShareApplication.class, args);
